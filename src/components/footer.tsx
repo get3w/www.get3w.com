6e5002dbdebc6fdda7aebdd5f44c874b2d7b55f6
starlying <corp@siteserver.cn>
2016-04-07 11:29:36 +0800

    s

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,15 +3,49 @@ import * as ReactDOM from 'react-dom'
 
 class Footer extends React.Component<{}, {}> {
   render() {
-    return (
-      <div className="footerBg">
-        <div className="footer">
-          <a className="cor_bs" href="#">联系我们</a>
-          &nbsp; 中国移动网上党校版权所有 &nbsp; 京ICP备05938475号
+        return (
+<footer className="footer">
+    <div className="ct-container">
+        <div className="ct-row">
+            <div className="ct-md-3 ct-sm-12">
+                <a href="/" className="footer-logo">
+                  <img src="/assets/img/logo-grey.png" />
+                </a>
+            </div>
+            <nav className="ct-md-2 ct-sm-6">
+                <h4 className="footer-title">Explore</h4>
+                <a href="/">Most Popular</a>
+                <a href="/">Most Stars</a>
+                <a href="/">Recently Added</a>
+            </nav>
+            <nav className="ct-md-2 ct-sm-6">
+                <h4 className="footer-title">Social</h4>
+                <a href="https://www.facebook.com/get3w/" target="_blank">Facebook</a>
+                <a href="https://www.twitter.com/get3w/" target="_blank">Twitter</a>
+                <a href="https://www.pinterest.com/get3w/" target="_blank">Pinterest</a>
+                <a href="https://github.com/get3w/" target="_blank">Github</a>
+            </nav>
+            <nav className="ct-md-2 ct-sm-6">
+                <h4 className="footer-title">Quick Links</h4>
+                <a href="/">Contact</a>
+                <a href="/">We're Hiring!</a>
+            </nav>
+            <nav className="ct-md-3 ct-sm-6">
+                <h4 className="footer-title">About</h4>
+                <p>
+                  Get3W is a hackable site editor for the 21st century, with only one mission: Make website more simplified.
+                </p>
+            </nav>
         </div>
-      </div>
-    )
-  }
+    </div>
+    <div className="ct-container">
+        <div className="footer-legal">
+            © 2016 GET3W.COM
+        </div>
+    </div>
+</footer>
+            )
+    }
 }
 
 export default Footer
--- src/containers/header.tsx
+++ src/containers/header.tsx
@@ -20,38 +20,124 @@ interface P {
 }
 
 interface S {
-  isRight: boolean
+  query: string
+  isMenu: boolean
 }
 
 class Header extends React.Component<P, S> {
   constructor(props: P) {
     super(props)
     this.state = {
-      isRight: false,
+      query: utils.Page.getUrlVar('q'),
+      isMenu: false,
     }
   }
 
-  onRightClick(e: React.MouseEvent) {
+  onSearch(e: React.MouseEvent) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    if (this.state.query) {
+      utils.Page.redirect('')
+    }
+  }
+
+  onSingoutClick(e: React.MouseEvent) {
+    //utils.Auth.signout()
+  }
+
+  onMenu(e: React.MouseEvent) {
     this.setState({
-      isRight: !this.state.isRight
+      query: this.state.query,
+      isMenu: !this.state.isMenu
     })
   }
 
-  onOrgChange(org: models.Org, isAdmin: boolean, isLeader: boolean, title: string, e: React.MouseEvent) {
-
-  }
-
   render() {
+    var btnElement = null
+    if (this.props.authState.isAnonymous) {
+      btnElement = [
+        <a key="signup" className="signup" href={utils.Addr.getSignupUrl() }>Sign up</a>,
+        <a key="login" className="login" href={utils.Addr.getLoginUrl() }>Login</a>
+      ]
+    } else {
+      var username = this.props.authState.user.username
+      var userUrl = ''//webUtils.getUserUrl(username)
+      var starsUrl = ''//webUtils.getStarsUrl(username)
+      var menuEl = null
+      if (this.state.isMenu) {
+        menuEl = (
+          <ul className="dropdown-menu">
+            <li className="dropdown-header">Welcome</li>
+            <li className="divider" />
+            <li><a href={userUrl}>Your apps</a></li>
+            <li><a href={starsUrl}>Your stars</a></li>
+            <li className="divider" />
+            <li><a href="">Create new...</a></li>
+            <li className="divider" />
+            <li><a href="">Settings</a></li>
+            <li><a href="javascript:void(0)" onClick={this.onSingoutClick.bind(this) }>Sign out</a></li>
+          </ul>
+        )
+      }
+      btnElement = (
+        <div className="user">
+          <a className="username" href="javascript:void(0)" onClick={this.onMenu.bind(this) }>
+            {username}
+            <i className="fa fa-caret-down" />
+          </a>
+          {menuEl}
+        </div>
+      )
+    }
 
-    return (
-      <div className="header">
-        <a className="logo" href="#"><img src="/assets/images/logo.jpg" width="351" height="49" /></a>
-        <div className="headSel">
-         <div className="headNm" onClick={this.onRightClick.bind(this)}>
-         &nbsp;
-         </div>
+    let sloganEl = null
+    sloganEl = (
+      <div className="ct-container carrousel">
+        <div className="ico">
+          <img src="/assets/img/features/logo-large.png" />
+        </div>
+        <p className="solgan">
+          Suggest Edits for the World Wide Web
+        </p>
+        <p className="sub-slogan">
+          you can change any websites with Get3W Suggest Edits, your suggestions will saved here until the website owner approves them.
+        </p>
+        <div className="start-button">
+          <a href="/signup/index.html?returnUrl=%2F" className="g3-btn register-btn">
+            <span>Sign up for free</span>
+            <i className="fa fa-angle-right"></i>
+          </a>
         </div>
       </div>
+    )
+
+    return (
+      <header className="header liner-bg">
+      <div className="ct-container">
+        <div className="navigation">
+          <div className="navbar-header">
+            <a className="logo ct-fl" href="/"><img src="/assets/img/logo.png" alt="logo" /></a>
+            <a className="navbar-toggle ct-fr" href="/"><i className="fa fa-reorder" /></a>
+          </div>
+          <div className="navbar-collapse collapse">
+            <nav className="nav">
+              <a href="" style={{display: 'none'}}>Features</a>
+              <a href="">Explore</a>
+              <a href="">Pricing</a>
+            </nav>
+            <div className="navbar-right">
+              <form className="input-form" onSubmit={this.onSearch.bind(this) }>
+                <input value={utils.Page.getUrlVar('q')} className="input-text" />
+                <a className="fa fa-search" href="javascript:void(0)" onClick={this.onSearch.bind(this) }></a>
+              </form>
+              {btnElement}
+            </div>
+          </div>
+        </div>
+      </div>
+      {sloganEl}
+        </header>
     )
   }
 }
